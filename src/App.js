7ab76512273da63086574aaa65b7a8ec61ea58df
fermorelli <fer.cmorelli@gmail.com>
2023-04-04 15:14:22 -0300

    changed api, shorted navigation, getting data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { Home } from './components/Home/Home';
 import { Weather } from './components/LocationWeather/Weather';
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <Weather />
     </div>
   );
--- src/components/LocationWeather/Weather.jsx
+++ src/components/LocationWeather/Weather.jsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const Weather = () => {
-
-    const input = 'Berlin';
+    const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
+    const [buttonClicked, setButtonClicked] = useState(false);
 
     const options = {
         method: 'GET',
         url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-        params: {q: `${input}`, days: '7'},
+        params: {q: `${city}`, days: '7'},
         headers: {
           'X-RapidAPI-Key': '434eb77b37msh2bd772753bd99c7p18b85bjsnefe11ac8e3a4',
           'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
@@ -16,14 +17,37 @@ export const Weather = () => {
       };
 
     useEffect(()=>{
-        axios.request(options).then(function (response) {
-            console.log(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
-    }, [])
+        if(buttonClicked){
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+                setWeatherData(response.data);
+            }).catch(function (error) {
+                console.error(error);
+            });
+            setButtonClicked(false);
+        }
+    }, [buttonClicked])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setButtonClicked(true);
+      };
 
     return (
-        <div>Your city weather</div>
+        <>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="">Search your location</label>
+            <input type="text" value={city} onChange={(e)=> setCity(e.target.value)} />
+            <button type="submit">Go</button>
+        </form>
+        <div>
+            {weatherData ?
+                <>
+                <span>{`${city} weather:`}</span>
+                <span>{weatherData.location.name}</span>
+                </>
+            : <span>Make your search</span>}
+        </div>
+        </>
     )
 };
